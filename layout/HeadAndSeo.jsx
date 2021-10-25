e3c8a1b43e738ecbda025535c246b2a1c9c80012
Daniel Michaels <32674032+danielmichaels@users.noreply.github.com>
2021-10-25 14:16:36 +1100

    Add favicon, banner and update og:tags

--- layout/HeadAndSeo.jsx
+++ layout/HeadAndSeo.jsx
@@ -3,26 +3,26 @@ import Head from 'next/head'
 
 const domain = process.env.DOMAIN_NAME
 export default function HeadAndSeo({page_title}) {
-  const title = page_title ? `Shorty | ${page_title}` : "Shorty"
+  const title = page_title ? `Tars.run | ${page_title}` : "Tars.run"
   return (
     <>
       <Head>
-        <link rel="icon" href="/assets/icons/favicon.png"/>
+        <link rel="icon" href="/assets/icons/favicon.ico"/>
       </Head>
       <NextSeo
         title={title}
-        description=""
-        canonical=domain
+        description="A simple no-frills URL shortener for hobbyists. Open-source and free to use."
+        canonical={domain}
         openGraph={{
           url: domain,
-          title: '',
-          description: "",
+          title: 'Tars.run',
+          description: "A simple no-frills URL shortener",
           images: [
             {
-              url: '',
+              url: '/banner.png',
               width: 800,
               height: 600,
-              alt: ""
+              alt: "Tars.run is a dead simple URL shortener"
             }
           ],
           site_name: domain,
